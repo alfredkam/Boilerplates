31f632137395c71554f43306a42e512709611ffb
alfredkam <kam.alfred@gmail.com>
2012-12-22 16:43:01 -0500

    added boilerplate

---
+++ js/boilerplate.js
@@ -0,0 +1,14 @@
+//boilerplate 
+//this file is a common js template 
+
+(function() {
+	define([
+		'jquery/jquery',
+		'underscore/underscore',
+		'backbone/backbone',
+		'mustache/mustache'
+	],function($, _, Backbone, Mustache) {
+	
+
+	});
+})();
