79e0d8ed18bd562d71205a5f7333abdd0f9761ce
alfredkam <kam.alfred@gmail.com>
2012-12-23 11:41:13 -0500

    added select tab code

--- js/presenter/router.js
+++ js/presenter/router.js
@@ -6,7 +6,7 @@
 		'backbone/backbone',
 		'mustache/mustache',
 		'views/site.view',
-		'text!../tpl/footer.mustache',
+		'text!tpl/footer.mustache',
 	],function($, _, Backbone, Mustache, Views, tpl) {
 		var Routes = Backbone.Router.extend({
 			//routes setup
@@ -22,28 +22,44 @@
 
 		//depending on the routes condition 
 		var init = function() {
+			var selectTab = function(tab) {
+				$(".nav li").each(function() {
+					$(this).removeClass("active");
+				});
+				
+				$(".nav li").each(function() {
+					if($(this).children("a").attr("href").trim() == "#" && tab == "_default") 
+						$(this).addClass("active");
+					else if($(this).children("a").attr("href").toLowerCase().search(tab.toLowerCase()) > -1)
+						$(this).addClass("active");
+				});		
+			};
+
 			var route = new Routes;
-			
 			route.on('route:about', function(lang) {
 				if(!lang) 
 					lang = "EN";
+				selectTab("about");
 				var aboutView = new Views.About;
 				aboutView.render(lang);
 			});	
 			route.on('route:contact', function(lang) {
 				if(!lang) 
 					lang = "EN";
+				selectTab("contact");
 				var contactView = new Views.Contact;
 				contactView.render(lang);
 			});	
 			route.on('route:anotherDefaultRoute', function(lang) {
 				if(!lang) 
 					lang = "EN";
+				selectTab("_default");
 				var defaultView = new Views.Default;
 				defaultView.render(lang);
 			});
 
 			route.on('route:defaultRoute', function() {
+				selectTab("_default");
 				var defaultView = new Views.Default;
 				defaultView.render("EN");
 			});
