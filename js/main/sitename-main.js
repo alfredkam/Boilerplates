602fe3c29fc126b9903b190f115d27145e8972ad
alfredkam <kam.alfred@gmail.com>
2013-05-06 15:03:32 -0700

    included additional frameworks

--- js/main/sitename-main.js
+++ js/main/sitename-main.js
@@ -6,12 +6,17 @@ require.config({
 		//Require Plugins
 		text: "lib/require/text",
 		
-		//Others
+		//Core API / Framework / Plugins
 		json: "lib/json/json2",
 		jquery: "lib/jquery/jquery-1.9.1",
 		underscore: "lib/underscore/underscore",
 		backbone: "lib/backbone/backbone",
-		mustache: "lib/mustache/mustache"
+		mustache: "lib/mustache/mustache",
+		marionette : "/lib/marionette/marionette.min",
+		"backbone.babysitter" : "/lib/backbone.babysitter/Backbone.babysitter",
+		"backbone.wreqr" : "/lib/backbone.Wreqr/Backbone.Wreqr"
+		//TODO : include others here if you want to use them
+		//"bootstrap" : "/lib/bootstrap/bootstrap.min"
 	},
 	
 	/*
@@ -19,28 +24,32 @@ require.config({
 	 * It is also used to define dependencies for scripts that DO follow the module style.
 	 */
 	shim: {
-		
 		json: {
 			exports: "JSON"
 		},
-		
 		jquery: ["json"],
 		
 		underscore: {
 			deps: ["json"],
 			exports: "_"
 		},
-		
 		backbone: {
 			deps: ["json", "underscore", "jquery"],
 			exports: "Backbone"
 		},
-		
-		mustache: ["json"]
+		mustache: ["json"],
+		marionette : {
+			deps : ["json", "jquery", "underscore","backbone"],
+			exports : "Marionette"
+		}
+		/*
+		bootstrap : {
+			deps : ["jquery"]
+		}*/
 	}
 });
 
-require(["js/util/WebConsoleUtils"], function(WebConsoleUtils){
+require(["js/util/WebConsoleUtils", "backbone", "js/Router"], function(WebConsoleUtils){
 	
 	//TODO: Starting point
 	var config = window.sitename.getConfig();
@@ -50,6 +59,9 @@ require(["js/util/WebConsoleUtils"], function(WebConsoleUtils){
 	var consoleUtils = new WebConsoleUtils({
 		//debug: debug flag here
 	});
+	
+	var router = new Router();
+	Backbone.history.start();
 	
 	consoleUtils.initConsole(/*debgu flag here*/);
 });
