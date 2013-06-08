55ccb48ea3a0198549f4039ffd0a42acf98320b6
alfredkam <kam.alfred@gmail.com>
2013-06-08 00:52:25 -0700

    + updated index to match with layout + add code to AppLayout

---
+++ Marionette/js/views/AppLayout.js
@@ -0,0 +1,18 @@
+// views.dashboard.DashboardLayout
+
+define([
+        "backbone",
+        "marionette",
+        "underscore"
+], function(Backbone, Marionette, _){
+	
+	var AppLayout = Marionette.Layout.extend({
+		regions: {
+			headerRegion: "#header-wrapper",
+			contentRegion: "#app-content",
+			footerRegion: "#footer"			
+		}
+	});
+	
+	return AppLayout;
+});
